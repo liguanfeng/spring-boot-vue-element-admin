5a3bb0c3ab1f4f75d989c7cf46dd7ed12f449bf7
udream-ligf <li.guan.feng@qq.com>
2020-05-19 17:50:14 +0800

    no comment

--- admin/src/main/java/com/yy/admin/controller/MenuController.java
+++ admin/src/main/java/com/yy/admin/controller/MenuController.java
@@ -1,11 +1,21 @@
 package com.yy.admin.controller;
 
+import com.yy.admin.AdminApplication;
+import com.yy.admin.annotations.Authorization;
 import com.yy.admin.entity.Menu;
 import com.yy.admin.service.MenuService;
 import com.yy.admin.vo.Result;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/menu")
@@ -14,9 +24,39 @@ public class MenuController {
 
     @Autowired
     private MenuService menuService;
+    @Autowired
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
     /**
      * 菜单列表
+     *
+     * @return
+     */
+    @RequestMapping("/request_path_list")
+    @Authorization(checkPrivilege = false)
+    public Result apiPathList(String path) {
+        String basePack = AdminApplication.class.getPackage().getName();
+        Collection<String> pathList = new TreeSet<>();
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = requestMappingHandlerMapping.getHandlerMethods();
+        handlerMethods.forEach((k, v) -> {
+            Set<String> patterns = k.getPatternsCondition().getPatterns();
+            patterns.forEach(pattern -> {
+                Authorization auth = v.getMethodAnnotation(Authorization.class);
+                if (v.getBeanType().getName().startsWith(basePack) && (auth == null || auth.checkPrivilege())) {
+                    pathList.add(pattern);
+                }
+            });
+        });
+        if (!StringUtils.isEmpty(path)) {
+            List<String> list = pathList.stream().filter(item -> item.contains(path) || PatternMatchUtils.simpleMatch(path, item)).collect(Collectors.toList());
+            return new Result(list);
+        }
+        return new Result(pathList);
+    }
+
+    /**
+     * 菜单列表
+     *
      * @return
      */
     @RequestMapping("/list")
@@ -35,8 +75,6 @@ public class MenuController {
         menuService.save(menu);
         return new Result();
     }
-
-
 
 
 }
